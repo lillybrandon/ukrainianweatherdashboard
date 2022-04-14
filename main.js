cd26af1029d82c4d0703a02e8c320945c9b882f7
Kobe Lovan <kobelovan7@gmail.com>
2022-04-13 21:20:47 -0500

    added JS and class to kyiv

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-https://api.openweathermap.org/data/2.5/weather?lat=50.4501&lon=30.5234&appid=23d1d98550e7ba39917485022c6515ec
+let weather = {
+    apiKey: "23d1d98550e7ba39917485022c6515ec",
+    fetchWeather: function (city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid=23d1d98550e7ba39917485022c6515ec"
+        ).then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = (data);
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed)
+        document.querySelector(".kyiv").innerText = "Current Weather " + temp;
+        document.querySelector(".icon").src = "openweathermap.org/img/wn/" + icon + ".png ";
+    }
+}
